f7d684f3985b08ecce07255a97519268dffa4d4a
brad.jordan <brad.jordan@cloudability.com>
2017-01-03 16:34:07 -0700

    added search

--- src/ContactList.js
+++ src/ContactList.js
@@ -9,8 +9,12 @@ class ContactList extends Component {
             search: '',
             contacts: props.contacts
         };
+
     }
 
+    updateSearch(event) {
+        this.setState({search: event.target.value.substr(0, 20)});
+    }
 
     render() {
 
@@ -21,6 +25,9 @@ class ContactList extends Component {
 
         return (
             <div>
+                <input type="text" placeholder="Search" value={this.state.search}
+                       onChange={this.updateSearch.bind(this)}/>
+
                 {filteredContacts.map((contact) => {
                     return <Contact contact={contact} key={contact.id}/>
                 })}
