a6ea6726f4b064c8f848b963f3ac01d33afe978e
brad.jordan <brad.jordan@cloudability.com>
2017-01-06 12:03:05 -0700

    added title and graph legend

--- src/Histogram.js
+++ src/Histogram.js
@@ -4,10 +4,17 @@ import ReactHighcharts from 'react-highcharts';
 import Highlight from 'react-highcharts';
 
 var config = {
+    title: {
+        text: 'Logins'
+    },
+    subtitle: {
+        text: '(30 days)'
+    },
     xAxis: {
         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     },
     series: [{
+        name: '(Any Account)',
         data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
     }]
 };
