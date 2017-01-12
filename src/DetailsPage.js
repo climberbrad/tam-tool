728827624361a57081dbe97d38e2cc6516c026a5
brad.jordan <brad.jordan@cloudability.com>
2017-01-12 15:40:13 -0700

    added spend per service graph

--- src/DetailsPage.js
+++ src/DetailsPage.js
@@ -14,9 +14,14 @@ export default class DetailsPage extends Component {
     render() {
         return (
             <div>
-                <div id="leftcolumn"><Sidebar org={this.state.org}/></div>
-                <div id="centercolumn"><HighChartComponent org={this.state.org} graphType="spend"/></div>
-                <div id="rightcolumn"><HighChartComponent org={this.state.org} graphType="logins"/></div>
+                <div>
+                    <div id="leftcolumn"><Sidebar org={this.state.org}/></div>
+                    <div id="content">
+                        <div id="centercolumn"><HighChartComponent org={this.state.org} graphType="spend"/></div>
+                        <div id="rightcolumn"><HighChartComponent org={this.state.org} graphType="logins"/></div>
+                        <div id="bottomcolumn"><HighChartComponent org={this.state.org} graphType="spendPerService"/></div>
+                    </div>
+                </div>
             </div>
         );
     }
