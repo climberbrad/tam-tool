db13e82446961dd3ee5bf85f5e3c0896d0d8994a
brad.jordan <brad.jordan@cloudability.com>
2017-01-11 23:03:51 -0700

    css formatting

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -23,30 +23,33 @@ export default class Sidebar extends Component {
 
     render() {
         return (
-        <div>
-            <div id="organization_name">{this.state.org.name}</div>
+            <div className="side-bar">
+                <div id="organization_name">{this.state.org.name}</div>
 
-            <h3><a href="">Activity</a></h3>
-            <div id="#org_stat">Last Login: Today</div>
-            <div id="#org_stat">Logins (past 30 days): 17</div>
-            <div id="#org_stat">Logins (past 60 days): 56</div>
-            <div id="#org_stat">Total accounts: {this.state.orgDetails.numAccounts}</div>
-            <h3><a href="">Monthly Spend</a></h3>
-            <div id="#org_stat">MRR: $8,000</div>
-            <div id="#org_stat">EC2: $100</div>
-            <div id="#org_stat">RDS: $17</div>
-            <div id="#org_stat">Redshift: $56</div>
-            <div id="#org_stat">Elasticache: $6</div>
-            <h3><a href="">Reservations</a></h3>
-            <div id="#org_stat">Total Reservations: {this.state.orgDetails.numReservations}</div>
-            <div id="#org_stat">Savings from plan: $67,874</div>
-            <div id="#org_stat">Savings from mods: $874</div>
-            <div id="#org_stat">Last RI purchase: 2017-01-03</div>
-            <div id="#org_stat">Next RI to expire: 2017-03-15</div>
-            <div id="#org_stat">Total instances: 876</div>
-            <div id="#org_stat">Percent tagged: 34%</div>
-            <div id="#org_stat">cldy score: 17</div>
-        </div>
+                <div className="side-bar-header"><a href="">Activity</a></div>
+                <div id="org_stat">Last Login: Today</div>
+                <div id="org_stat">Logins (past 30 days): 17</div>
+                <div id="org_stat">Logins (past 60 days): 56</div>
+                <div id="org_stat">Total accounts: {this.state.orgDetails.numAccounts}</div>
+
+                <div className="side-bar-header"><a href="">Monthly Spend</a></div>
+                <div id="org_stat">MRR: $8,000</div>
+                <div id="org_stat">EC2: $100</div>
+                <div id="org_stat">RDS: $17</div>
+                <div id="org_stat">Redshift: $56</div>
+                <div id="org_stat">Elasticache: $6</div>
+
+                <div className="side-bar-header"><a href="">Reservations</a></div>
+                <div id="org_stat">Total Reservations: {this.state.orgDetails.numReservations}</div>
+                <div id="org_stat">Savings from plan: $67,874</div>
+                <div id="org_stat">Savings from mods: $874</div>
+                <div id="org_stat">Last RI purchase: 2017-01-03</div>
+                <div id="org_stat">Next RI to expire: 2017-03-15</div>
+                <div id="org_stat">Total instances: 876</div>
+                <div id="org_stat">Percent tagged: 34%</div>
+                <div id="org_stat">cldy score: 17</div>
+                <br/>
+            </div>
         )
     }
 }
