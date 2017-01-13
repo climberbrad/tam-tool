d41403628b82e4508461fcd978a534c6a67a86ef
brad.jordan <brad.jordan@cloudability.com>
2017-01-12 22:01:31 -0700

    added last planner executed date

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -34,21 +34,20 @@ export default class Sidebar extends Component {
                 <div id="org_stat">Total accounts: <div className="org-data">{this.state.orgDetails.numAccounts}</div></div>
 
                 <div className="side-bar-header">Monthly Spend</div>
-                <div id="org_stat">MRR: <div className="org-data">TBD</div></div>
                 <div id="org_stat">EC2: <div className="org-data">TBD</div></div>
                 <div id="org_stat">RDS: <div className="org-data">TBD</div></div>
                 <div id="org_stat">Redshift: <div className="org-data">TBD</div></div>
                 <div id="org_stat">Elasticache: <div className="org-data">TBD</div></div>
+                <div id="org_stat">MRR: <div className="org-data">TBD</div></div>
 
                 <div className="side-bar-header">Reservations</div>
                 <div id="org_stat">Total Reservations: <div className="org-data">{this.state.orgDetails.numReservations}</div></div>
                 <div id="org_stat">RI's to expire next month: <div className="org-data">{this.state.orgDetails.numRisExpiringNextMonth}</div></div>
                 <div id="org_stat">RI Planner Savings: <div className="org-data">${this.state.orgDetails.savingsFromPlan}</div></div>
                 <div id="org_stat">Last RI purchase: <div className="org-data">{this.state.orgDetails.dateOfLastRiPurchase}</div></div>
-                <div id="org_stat">Last planner run: <div className="org-data">$TBD</div></div>
+                <div id="org_stat">RI Planner executed: <div className="org-data">{this.state.orgDetails.planLastExecuted}</div></div>
                 <div id="org_stat">Savings from mods: <div className="org-data">$TBD</div></div>
-                <div id="org_stat">Total instances: <div className="org-data">TBD</div></div>
-                <div id="org_stat">Percent tagged: <div className="org-data">TBD</div></div>
+                <div id="org_stat">Tagged inventory percent: <div className="org-data">TBD%</div></div>
                 <div id="org_stat">cldy score: <div className="org-data">TBD</div></div>
                 <br/>
             </div>
